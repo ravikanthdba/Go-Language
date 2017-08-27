dbc9aa00e9282f4bdf03158b97f79c240db4cbc0
Ravikanth Garimella <rgarimella@linkedin.com>
2017-08-27 20:21:28 +0530

    Variables in Go

---
+++ Variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x string = "Hello World"
+    fmt.Println(x)
+    var int32_x int32 = 1234567890
+    var int64_x int64 = 123456789012345
+    fmt.Println(int32_x,int64_x)
+}
