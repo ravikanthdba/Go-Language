971eb6a0513a1d0470a8582a09e258be3be8030c
Ravikanth Garimella <rgarimella@linkedin.com>
2017-08-29 22:41:59 +0530

    Packages

---
+++ Packages/package1/name.go
@@ -0,0 +1,5 @@
+package package1
+
+// Since the variable "Name" below starts with a capital Letter, this variable can be exported
+
+var Name string = "Ravikanth Garimella"
---
+++ Packages/package1/namefromfunction.go
@@ -0,0 +1,13 @@
+package package1
+import "fmt"
+
+
+func GetName() string {
+    return fmt.Printf("The name from the Function is Ravikanth Garimella")
+}
+
+
+
+func GetNamefromParameters(name string) string {
+    return fmt.Printf("The name from the Function is %s", name)
+}
